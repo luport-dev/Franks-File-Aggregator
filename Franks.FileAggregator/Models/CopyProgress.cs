1cab0018524d66c9774087d52c75361cbbda9d0a
luport.dev <luport.dev@gmail.com>
2025-12-24 19:59:55 +0100

    Release v1.0 only

--- Franks.FileAggregator/Models/CopyProgress.cs
+++
@@ -1,9 +0,0 @@
-namespace Franks.FileAggregator.Models;
-
-public sealed class CopyProgress
-{
-    public int CurrentIndex { get; init; }
-    public int Total { get; init; }
-    public string FileName { get; init; } = string.Empty;
-    public double Percent { get; init; }
-}
